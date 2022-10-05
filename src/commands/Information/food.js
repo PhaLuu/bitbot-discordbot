e1116241b0c401552b25a331172fe9897fa30a37
PhaLuu <phaluu.aus@gmail.com>
2022-10-05 13:45:09 +1100

    edited food command

--- src/commands/Information/food.js
+++ src/commands/Information/food.js
@@ -1,10 +1,28 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
+async function getFood() {
+	try {
+		const food = await axios.get('https://api.edamam.com/api/recipes/v2?type=public&q=cake&app_id=e69c3227&app_key=a59e93b9409600bc19a90852d954cc19')
+		return food
+	} catch (error) {
+		console.log(error);
+	}
+}
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("food")
-		.setDescription("testing: food reciped...."),
+		.setDescription("This will get a random food recipe"),
 	async execute(interaction) {
-		await interaction.reply("https://api.edamam.com/api/recipes/v2?type=public&q=cake&app_id=e69c3227&app_key=a59e93b9409600bc19a90852d954cc19");
+		//console.log(interaction.options.getString('hexvalue'));
+		const food = await getFood()
+		const contentEmbed = new EmbedBuilder()
+		contentEmbed.setTitle('Random 🦊');
+		contentEmbed.setColor(0xFFA500);
+		console.log(food.data.image);
+		contentEmbed.setURL(food.data.hits[0].image)
+		contentEmbed.setImage(food.data.image)
+
+		await interaction.reply({ embeds: [contentEmbed] });
 	},
 };
